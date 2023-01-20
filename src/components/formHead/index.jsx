5f0c4b033e6e204a747ec4c0b0f2fc736096fac8
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-01-20 21:05:26 +0200

    edit

--- src/components/formHead/index.jsx
+++ src/components/formHead/index.jsx
@@ -1,10 +1,13 @@
 import style from "./Style.module.scss"
 
-export const FormHead = (props) => {
+export const FormHead = ({
+    title,
+    subTitle
+}) => {
     return (
         <div className={`${style.main}`}>
-            <div className={`${style.main__title}`}>{props.title}</div>
-            <div className={`${style.main__title_bub}`}>{props.subTitle}</div>
+            <div className={`${style.main__title}`}>{title}</div>
+            <div className={`${style.main__title_bub}`}>{subTitle}</div>
         </div>
     )
 }
