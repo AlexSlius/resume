27c27488a8622cca24eb90eebc2ab1130c961f39
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-05-11 22:06:28 +0300

    edit

--- src/containers/resume-now/index.jsx
+++ src/containers/resume-now/index.jsx
@@ -8,20 +8,8 @@ import { routersPages } from "../../constants/next-routers";
 
 import style from "./Style.module.scss"
 
-const arr = [
-    {
-        title: "Can I change my plan?",
-        text: "<p>Yes, you can upgrade, downgrade or cancel your plan at any time.</p> <p>Do it yourself in your account or contact us, we are here to help.</p>"
-    },
-    {
-        title: "Do I get full access with the trial?",
-        text: "<p>Yes, you can upgrade, downgrade or cancel your plan at any time.</p> <p>Do it yourself in your account or contact us, we are here to help.</p>"
-    },
-    {
-        title: "What about money back?",
-        text: "<p>Yes, you can upgrade, downgrade or cancel your plan at any time.</p> <p>Do it yourself in your account or contact us, we are here to help.</p>"
-    }
-];
+import arrFaqs from "./data/faqs.json";
+import arrCard from "./data/cards.json";
 
 const ResumeNow = () => {
     const {
@@ -42,83 +30,36 @@ const ResumeNow = () => {
                 <p className={`${style.sub_title}`}>To download your resume simply sign up for your Premium Membership. As an added bonus,
                     you’ll gain instant full access to our suite of expertly crafted career services.</p>
                 <div className={`${style.items_grid}`}>
-                    <div className={`${style.card}`}>
-                        <div>
-                            <div className={`${style.card_top}`}>
-                                <div className={style.head}>Low price</div>
-                            </div>
-                            <div className={style.car_center}>
-                                <div className={style.car_price}>
-                                    <span>$</span>399.00
+
+                    {
+                        arrCard.map((itemCard, index) => (
+                            <div className={`${style.card} ${(index == 1) ? style.active : ""}`} key={index}>
+                                <div>
+                                    <div className={`${style.card_top}`}>
+                                        <div className={style.head} style={{ color: itemCard.color }}>{itemCard.name}</div>
+                                    </div>
+                                    <div className={style.car_center}>
+                                        <div className={style.car_price}>
+                                            <span>$</span>{itemCard.price}
+                                        </div>
+                                        <div className={style.car_days}>{itemCard.day}</div>
+                                        <div className={style.car_wr_list}>
+                                            <ul className="list-check">
+                                                {
+                                                    itemCard.list.map((itemList, index) => (
+                                                        <li key={index}>{itemList}</li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className={style.car_days}>Forever</div>
-                                <div className={style.car_wr_list}>
-                                    <ul className="list-check">
-                                        <li>Unlimited PDF Downloads</li>
-                                        <li>Unlimited Resumes</li>
-                                        <li>Unlimited Cover Letters</li>
-                                        <li>Cancel any time</li>
-                                        <li>7-day money-back guarantee*</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={style.car_bot}>
-                            <button className={`bnt-now ${style.bnt_now}`}><span>Upgrade Now</span></button>
-                        </div>
-                    </div>
-                    <div className={`${style.card} ${style.active}`}>
-                        <div>
-                            <div className={`${style.card_top}`}>
-                                <div className={style.head}>Best sale</div>
-                            </div>
-                            <div className={style.car_center}>
-                                <div className={style.car_price}>
-                                    <span>$</span>1.95
-                                </div>
-                                <div className={style.car_days}>7-Day trial subscription</div>
-                                <div className={style.car_wr_list}>
-                                    <ul className="list-check">
-                                        <li>Unlimited PDF Downloads</li>
-                                        <li>Unlimited Resumes</li>
-                                        <li>Unlimited Cover Letters</li>
-                                        <li>After 7 days, auto-renews to $19.95</li>
-                                        <li>Billed every 4 weeks</li>
-                                        <li>Cancel any time</li>
-                                        <li>7-day money-back guarantee*</li>
-                                    </ul>
+                                <div className={style.car_bot}>
+                                    <button className={`bnt-now ${style.bnt_now}`}><span>Upgrade Now</span></button>
                                 </div>
                             </div>
-                        </div>
-                        <div className={style.car_bot}>
-                            <button className={`bnt-now ${style.bnt_now}`}><span>Upgrade Now</span></button>
-                        </div>
-                    </div>
-                    <div className={`${style.card}`}>
-                        <div>
-                            <div className={`${style.card_top}`}>
-                                <div className={`${style.head} ${style.head_g}`}>profit</div>
-                            </div>
-                            <div className={style.car_center}>
-                                <div className={style.car_price}>
-                                    <span>$</span>69.95
-                                </div>
-                                <div className={style.car_days}>12 months</div>
-                                <div className={style.car_wr_list}>
-                                    <ul className="list-check">
-                                        <li>Unlimited PDF Downloads</li>
-                                        <li>Unlimited Resumes</li>
-                                        <li>Unlimited Cover Letters</li>
-                                        <li>Cancel any time</li>
-                                        <li>7-day money-back guarantee*</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={style.car_bot}>
-                            <button className={`bnt-now ${style.bnt_now}`}><span>Upgrade Now</span></button>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
                 <div className={`${style.cont}`}>
                     <div className={`${style.items_payment}`}>
@@ -139,7 +80,7 @@ const ResumeNow = () => {
                         <p className={`${style.sub_title}`}>Didn't find what you're looking for? — <Link href={`/${routersPages['faqs']}`} target="_blank">Use our FAQ</Link></p>
                     </div>
                     <div className={`${style.wr_accar}`}>
-                        <AccordionComponent arr={arr} />
+                        <AccordionComponent arr={arrFaqs} />
                     </div>
                 </div>
             </div>
