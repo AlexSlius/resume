bc73557ed11d7860b9f4fba8c00e22198e00804f
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-06-01 20:49:12 +0300

    edit

--- src/containers/shareResume/index.jsx
+++ src/containers/shareResume/index.jsx
@@ -1,5 +1,7 @@
 import Templates from "../templates";
 
+import style from "./style.module.scss";
+
 const ShareResume = ({
     isCover = true,
     ctx
@@ -7,9 +9,11 @@ const ShareResume = ({
     let isPdf = (ctx?.router.state?.query?.download == 'pdf') || (ctx?.router?.query?.download == 'pdf');
 
     return (
-        <div className={`page-share ${isPdf ? "pdf" : ""}`}>
-            <div className="page-share__container">
-                <Templates isCover={isCover} isPageView={true} />
+        <div className={isPdf ? style.wr_div : ""}>
+            <div className={`page-share ${isPdf ? "pdf" : ""}`}>
+                <div className="page-share__container">
+                    <Templates isCover={isCover} isPageView={true} />
+                </div>
             </div>
         </div>
     )
