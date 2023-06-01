3eace281122804d008af6f059a7487d77871b1ca
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-06-01 11:55:51 +0300

    edit

--- src/containers/shareResume/index.jsx
+++ src/containers/shareResume/index.jsx
@@ -7,12 +7,14 @@ const ShareResume = ({
     isCover = true,
     ctx
 }) => {
-    let isPdf = ctx?.router.state?.query?.download == 'pdf';
+    let isPdf = (ctx?.router.state?.query?.download == 'pdf');
 
     console.log("ctxpage: ", ctx);
 
     console.log("ctx?.router: ", ctx?.router.state?.query);
     console.log("ctx?.router.state?.query?.download: ", ctx?.router.state?.query?.download);
+
+    console.log("isPdf: ", isPdf);
 
     return (
         <div className={`page-share ${isPdf ? "pdf" : ""}`}>
