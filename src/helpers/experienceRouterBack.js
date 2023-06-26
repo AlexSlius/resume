047d9da952833a48204a5b4e3051066ae5472168
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-06-26 21:25:50 +0300

    ed

--- src/helpers/experienceRouterBack.js
+++ src/helpers/experienceRouterBack.js
@@ -6,17 +6,6 @@ import { QUERY_TAB_COVER } from "../constants/routes";
 export const backRoter = (arrObj, step, idCv, dataForm) => {
     let nameStapUrl = '';
     let baseUrl = `/${routersPages['coverLetter']}/${idCv}`;
-    let {
-        // step 2
-        questionGraduateFromCollege,
-        // step 3 on the 6
-        questionCurrentlyInCollegeUniversity,
-        // step 11
-        questionHaveWorkExperience,
-        // step 14
-        questionCurrentlyWorking
-    } = dataForm;
-
 
     if (!isObject(arrObj))
         return nameStapUrl;
@@ -28,17 +17,17 @@ export const backRoter = (arrObj, step, idCv, dataForm) => {
         nameStapUrl = arrObj[keys[(index - 1)]];
     }
 
-    if ((step == arrObj['professionalSkills']) && questionCurrentlyInCollegeUniversity == "N") {
+    if ((step == arrObj['professionalSkills']) && dataForm?.questionCurrentlyInCollegeUniversity == "N") {
         // transfer to the second step
         nameStapUrl = arrObj["graduated"];
     }
 
-    if ((step == arrObj['graduatedFinish']) && questionHaveWorkExperience == "N") {
+    if ((step == arrObj['graduatedFinish']) && dataForm?.questionHaveWorkExperience == "N") {
         // transfer to the second step
         nameStapUrl = arrObj["workExperinence"];
     }
 
-    if ((step == arrObj['graduatedTwo']) && questionCurrentlyWorking == "N") {
+    if ((step == arrObj['graduatedTwo']) && dataForm?.questionCurrentlyWorking == "N") {
         // transfer to the second step
         nameStapUrl = arrObj["graduatedStep"];
     }
