2b768bb021cb862b40af95f3f0e454b689383bc1
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-06-30 10:23:19 +0300

    edit show scroll

--- src/containers/templates/index.jsx
+++ src/containers/templates/index.jsx
@@ -553,7 +553,7 @@ const Templates = ({
                     }
 
                     {/* hide_scroll */}
-                    <div className="ptr-c scroll-style hide_scroll">
+                    <div className="ptr-c scroll-style">
                         <div className="ptr-c__content">
                             <div className="body-template-resume" style={{ transform: `scale(${useScaleResumePageShare()})` }}>
                                 {
