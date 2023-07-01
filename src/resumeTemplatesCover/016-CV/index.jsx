ae091b01aa3f31ecdcd1c387091d6bd2e94e0f4b
mprychyna <m.prychyna@astoundcommerce.com>
2023-07-01 13:09:18 +0300

    Resume 016 Cover recreating

--- src/resumeTemplatesCover/016-CV/index.jsx
+++ src/resumeTemplatesCover/016-CV/index.jsx
@@ -18,16 +18,17 @@ const drawing = () => {
 
             let text1 = getCvLetterContainer().find('.cv-letter-text');
 
-            if (getPageContainer2().height() > (getPageContainer2().parent().height())) {
+            if (getPageContainer2().outerHeight() > (getPageContainer2().parent().outerHeight())) {
                 do {
                     text1.html(text1.html().substring(0, text1.html().lastIndexOf(" ")));
+                    text1.html(text1.html().substring(0, text1.html().lastIndexOf("<")));
                 }
-                while (getPageContainer2().height() > (getPageContainer2().parent().height()));
+                while (getPageContainer2().outerHeight() > (getPageContainer2().parent().outerHeight()));
 
                 letter_current_page_number++;
                 getCvLetterContainer().append(original_cv_letter_text.clone());
                 let text2 = getCvLetterContainer().find('.cv-letter-text');
-                text2.html(text2.html().substring(text1.html().length));
+                text2.html(original_cv_letter_text.html().substring(text1.html().length - 4, original_cv_letter_text.html().length));
             }
         }
 
@@ -89,31 +90,33 @@ export const CoverCv016 = ({
     return (
         <div className="sv_016 template-wrapper" ref={reportTemplateRef}>
             <div id="cv-chapter-section-resume" className={`${stateClasses} cv-chapter-section color-scheme-state-color-set-1`} data-chapter="resume">
-                <div id="cv-body-2" data-chapter="resume" data-page="1" className="cv-body cv-body-2 cv-body---resume page-2 font-size-scheme-1 line-height-scheme-1">
-                    <div className="cv-body-content">
+                <div id="cv-body-2" data-chapter="resume" data-page="1" className="cv-body cv-body-2">
+                    <div className="cv-body-content font-size-1 main-color-2-text">
                         <div className="circle-shade"></div>
                         <div className="top-area">
-                            <h1 className="cv-name font-size-6 line-height-8 main-color-2-text font-weight-400">{firstName}{` `} {lastName}</h1>
+                            <h1 className="cv-name font-size-6">{firstName}{` `} {lastName}</h1>
                         </div>
                         <div className="middle-area">
                             <div className="column-left">
                                 <div className="destination-block block-block">
                                     <div className="block-to">
-                                        <h3 className="heading-type-1 font-size-4 line-height-6 main-color-2-text font-weight-300">To</h3>
-                                        <div className="underheading-line"></div>
-                                        <p className="font-size-1 line-height-2 main-color-2-text font-weight-300" dangerouslySetInnerHTML={{ __html: data.to }}></p>
+                                        <div className="heading-wrapper">
+                                            <h3 className="cv-heading font-size-4">To</h3>
+                                        </div>
+                                        <p dangerouslySetInnerHTML={{ __html: data.to }}></p>
                                     </div>
                                     <div className="block-from">
-                                        <h3 className="heading-type-1 font-size-4 line-height-6 font-weight-300">From</h3>
-                                        <div className="underheading-line"></div>
-                                        <p className="font-size-1 line-height-2 main-color-2-text font-weight-300" dangerouslySetInnerHTML={{ __html: data.from }}></p>
+                                        <div className="heading-wrapper">
+                                            <h3 className="cv-heading font-size-4">From</h3>
+                                        </div>
+                                        <p dangerouslySetInnerHTML={{ __html: data.from }}></p>
                                     </div>
                                 </div>
                             </div>
                             <div className="column-right">
                                 <div className="letter-block">
                                     {/* <h3 className="letter-heading font-size-2 cv-letter-heading line-height-3 main-color-2-text font-weight-400">{!!applyingCompanyTitle && (`Dear ${applyingCompanyTitle}`)} {!!applyingCompanyContact && (<>{applyingCompanyContact},</>)}</h3> */}
-                                    <div className="letter-text cv-letter-text font-size-1 line-height-2 main-color-2-text font-weight-300" dangerouslySetInnerHTML={{ __html: data.coverGenerateDate }}></div>
+                                    <div className="letter-text cv-letter-text" dangerouslySetInnerHTML={{ __html: data.coverGenerateDate }}></div>
                                 </div>
                             </div>
                         </div>
