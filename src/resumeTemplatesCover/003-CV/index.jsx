89265dd7a5f6e581c778fb94e8cc010e69ea2187
mprychyna <m.prychyna@astoundcommerce.com>
2023-08-18 00:14:53 +0300

    Cover 003 fix

--- src/resumeTemplatesCover/003-CV/index.jsx
+++ src/resumeTemplatesCover/003-CV/index.jsx
@@ -17,7 +17,7 @@ const drawing = () => {
 
       let text1 = getCvLetterContainer().find('.cv-letter-text');
 
-      if (getPageContainer2().height() > (getPageContainer2().parent().height())) {
+      if (Math.round(getPageContainer2().height()) > Math.round(getPageContainer2().parent().height())) {
         do {
           text1.html(text1.html().substring(0, text1.html().lastIndexOf(" ")));
           text1.html(text1.html().substring(0, text1.html().lastIndexOf("<")));
